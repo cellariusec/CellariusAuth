298388de1ec818babe5a179196af313cec31ae81
Anthony Josueth Cochea Conforme <anthony.cochea@udla.edu.ec>
2024-05-30 10:46:45 -0500

    create client

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -15,64 +16,62 @@ import (
 )
 
 func init() {
-    os.Setenv("DB_CONNECTION_STRING", "postgres://yjfgskzw:PRSNUNR2F8X8InPBIra5yi5xqozxMtx0@kala.db.elephantsql.com/yjfgskzw")
+	os.Setenv("DB_CONNECTION_STRING", "postgres://yjfgskzw:PRSNUNR2F8X8InPBIra5yi5xqozxMtx0@kala.db.elephantsql.com/yjfgskzw")
 	os.Setenv("ISSUER", "http://localhost:8080")
-	os.Setenv("SECRET","secret")
-	os.Setenv("JWT_SECRET","secret")
-	os.Setenv("AUDIENCE","http://localhost:5000")
-    initializer.LoadEnvVariables()
-    initializer.ConnectToDb()
-    initializer.SyncDatabase()
+	os.Setenv("SECRET", "secret")
+	os.Setenv("JWT_SECRET", "secret")
+	os.Setenv("AUDIENCE", "http://localhost:5000")
+	initializer.LoadEnvVariables()
+	initializer.ConnectToDb()
+	initializer.SyncDatabase()
 }
 
 func main() {
-  
-    blacklistDSN := "postgres://yjfgskzw:PRSNUNR2F8X8InPBIra5yi5xqozxMtx0@kala.db.elephantsql.com/yjfgskzw"
-    blacklistDB, err := gorm.Open(postgres.Open(blacklistDSN), &gorm.Config{})
-    if err != nil {
-        // Handle error
-        return
-    }
-    r := gin.Default()
 
-    config := cors.DefaultConfig()
-    config.AllowOrigins = []string{"http://localhost:3000", "https://verbose-orbit-ppxqpr7g9q6f6g4.github.dev","http://localhost:8080"} 
-    config.AllowCredentials = true
-    config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
-    config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Cookie", "Usertype"}
+	blacklistDSN := "postgres://yjfgskzw:PRSNUNR2F8X8InPBIra5yi5xqozxMtx0@kala.db.elephantsql.com/yjfgskzw"
+	blacklistDB, err := gorm.Open(postgres.Open(blacklistDSN), &gorm.Config{})
+	if err != nil {
+		// Handle error
+		return
+	}
+	r := gin.Default()
 
-    r.Use(cors.New(config))
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000", "https://verbose-orbit-ppxqpr7g9q6f6g4.github.dev", "http://localhost:8080"}
+	config.AllowCredentials = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Cookie", "Usertype"}
 
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    
-    r.OPTIONS("/signup", func(c *gin.Context) {
-   
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	r.Use(cors.New(config))
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
- 
-        c.AbortWithStatus(http.StatusOK)
-      })
-      //middleware.ValidateMiddleware
-    r.POST("/signup",controllers.Signup)
-    r.POST("/login", controllers.Login)
-    r.GET("/validate", controllers.Validate)
-    r.POST("/refresh-token", controllers.RefreshToken)
-    r.POST("/logout", controllers.Logout)
-    r.POST("/reset_password",middleware.ValidateEmailMiddleware, controllers.ResetPassword)
+	r.OPTIONS("/signup", func(c *gin.Context) {
 
-    ticker := time.NewTicker(1*time.Hour)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
-    go func(){
-        for range ticker.C{
-            util.DeleteExpiredTokens(blacklistDB)
-        }
-    }()
+		c.AbortWithStatus(http.StatusOK)
+	})
+	//middleware.ValidateMiddleware
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", controllers.Validate)
+	r.POST("/refresh-token", controllers.RefreshToken)
+	r.POST("/logout", controllers.Logout)
+	r.POST("/reset_password", middleware.ValidateEmailMiddleware, controllers.ResetPassword)
 
-    r.Run()  
+	ticker := time.NewTicker(1 * time.Hour)
+
+	go func() {
+		for range ticker.C {
+			util.DeleteExpiredTokens(blacklistDB)
+		}
+	}()
+
+	r.Run(":5050")
 }
